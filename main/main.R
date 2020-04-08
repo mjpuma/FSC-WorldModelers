fdd95cdfe6bed5be0ff0ceef1a7dfc1f24850e71
Puma <puma.nasa@gmail.com>
2020-04-08 02:55:31 -0400

    Update main.R
    flag to choose between PTA and RTA; issue remaining with multi-year RTA run

--- main/main.R
+++ main/main.R
@@ -19,6 +19,9 @@ if (dir.exists("outputs") == FALSE) {
 # rstudio version: Specify arguments  ====
 years <- 1:4  # Set year range to run model
 
+# Specify model version to run: 0-> PTA; 1-> RTA
+FSCversion = 0
+
 # Step 2: Load FSC functions ----
 source("main/FSC_component_funcs.R")
 source("main/FSC_sim_funcs.R")
@@ -58,36 +61,19 @@ Shocks <- Shocks[order(Shocks$FAO), ]
 Prod <- as.numeric(unlist(P0$P0))
 
 # Initialize  output vectors ====
-## PTA version
-P_PTA <-  array(0, c(nrow(country_list), length(years) + 1))
-R_PTA <-  array(0, c(nrow(country_list), length(years) + 1))
-E_PTA <-
+P <-  array(0, c(nrow(country_list), length(years) + 1))
+R <-  array(0, c(nrow(country_list), length(years) + 1))
+E <-
   array(0, c(nrow(country_list), nrow(country_list), length(years) + 1))
 
-shortage_PTA <- array(0, c(nrow(country_list), length(years)))
-C_C0_PTA <- array(0, c(nrow(country_list), length(years)))
-dR_C0_PTA <- array(0, c(nrow(country_list), length(years)))
-
-## RTA version
-P_RTA <-  array(0, c(nrow(country_list), length(years) + 1))
-R_RTA <-  array(0, c(nrow(country_list), length(years) + 1))
-E_RTA <-
-  array(0, c(nrow(country_list), nrow(country_list), length(years) + 1))
-
-shortage_RTA <- array(0, c(nrow(country_list), length(years)))
-C_C0_RTA <- array(0, c(nrow(country_list), length(years)))
-dR_C0_RTA <- array(0, c(nrow(country_list), length(years)))
+shortage <- array(0, c(nrow(country_list), length(years)))
+C_C0 <- array(0, c(nrow(country_list), length(years)))
+dR_C0 <- array(0, c(nrow(country_list), length(years)))
 
 ## Add initial conditions to output arrays
-## PTA version
-P_PTA[, 1] <- Prod
-R_PTA[, 1] <- R0
-E_PTA[, , 1] <- E0
-
-## RTA version
-P_RTA[, 1] <- Prod
-R_RTA[, 1] <- R0
-E_RTA[, , 1] <- E0
+P[, 1] <- Prod
+R[, 1] <- R0
+E[, , 1] <- E0
 
 ## Create 'Reserves' dataframe ====
 Reserves <- data_frame(iso3 = names(R0), R0 = R0)
@@ -113,8 +99,6 @@ for (i in 1:length(years)) {
   Shocks$dP <-
     (-FracLoss) * (Shocks$P0)  # adjust sign because fractional declines read in
   
-  dP <- as.numeric(unlist(Shocks$dP))
-  
   # Set Reserves and add POSTIVE anomalies to reserves ====
   if (i == 1) {
     ## First timestep, merge Shocks into Reserves dataframe
@@ -124,106 +108,66 @@ for (i in 1:length(years)) {
             by = 'iso3',
             all.x = TRUE,
             all.y = FALSE)
-    Reserves <- Reserves[order(Reserves$FAO), ]
+    Reserves <- Reserves[order(Reserves$FAO),]
     
     # Add positive anomalies to reserves ====
     Reserves$R1 <- Reserves$R0  + (Reserves$P0 * FracGain)
-    R1_PTA <- as.numeric(unlist(Reserves$R1))
-    R1_RTA <- as.numeric(unlist(Reserves$R1))
-    } else {
+    R1 <- as.numeric(unlist(Reserves$R1))
+  } else {
     # Update reserve levels: use ending levels from previous timestep and add production gains
-    R1_PTA <- results_FSC_PTA$R  + Reserves$P0 * FracGain
-    R1_PTA <- as.numeric(unlist(R1_PTA))
-    
-    #R1_RTA <- results_FSC_RTA$R  + Reserves$P0 * FracGain
-    #R1_RTA <- as.numeric(unlist(R1_RTA))
-    }
+    R1 <- results_FSC$R  + Reserves$P0 * FracGain
+    R1 <- as.numeric(unlist(R1))
+  }
   
-  # Update state variables for trade_dat and run PTA version ====
+  
+  # Update state variables for trade_dat ====
   if (i == 1) {
     # Assign production, reserves, and export matrix
-    trade_dat_PTA <- list(P = Prod, R = R1_PTA, E = E0)
+    trade_dat <- list(P = Prod, R = R1, E = E0)
     # Number of countries
-    trade_dat_PTA$nc <- length(trade_dat_PTA$P)
+    trade_dat$nc <-length(trade_dat$P)
     # Change in reserves
-    trade_dat_PTA$dR <- rep(0, trade_dat_PTA$nc)
-    # Initial shortage is 0
-    trade_dat_PTA$shortage <- rep(0, trade_dat_PTA$nc)
+    trade_dat$dR <-rep(0, trade_dat$nc)
     # Compute consumption assuming that it is initially equal to supply
-    trade_dat_PTA$C <- get_supply(trade_dat_PTA)
+    trade_dat$C <- get_supply(trade_dat)
+    # Initial shortage is 0
+    trade_dat$shortage <-
+      rep(0, trade_dat$nc)
     } else {
     # Clear trade_dat dataframe
-    rm(trade_dat_PTA)
+    rm(trade_dat)
     # Assign production, reserves *from previous timestep*, and export matrix
     #   i.e. update only reserves; leave consumption & trade at initial levels
-    trade_dat_PTA <- list(P = Prod, R = R1_PTA, E = E0)
+    trade_dat <- list(P = Prod, R = R1, E = E0)
     # Number of countries
-    trade_dat_PTA$nc <- length(trade_dat_PTA$P)
+    trade_dat$nc <- length(trade_dat$P)
     # Change in reserves
-    trade_dat_PTA$dR <- rep(0, trade_dat_PTA$nc)
+    trade_dat$dR <- rep(0, trade_dat$nc)
     # Initial shortage is 0
-    trade_dat_PTA$shortage <- rep(0, trade_dat_PTA$nc)
+    trade_dat$shortage <- rep(0, trade_dat$nc)
     # Compute consumption assuming that it is initially equal to supply
-    trade_dat_PTA$C <- get_supply(trade_dat_PTA)
+    trade_dat$C <- get_supply(trade_dat)
     # Clear results from previous timestep
-    rm(results_FSC_PTA)
+    rm(results_FSC)
     }
   
   # Call main simulation functions
-  results_FSC_PTA <- sim_cascade_PTA(trade_dat_PTA, dP)  # Run Proportional Trade Allocation (PTA) Model
-  
-  # Update state variables for trade_dat and run RTA version ====
-  if (i == 1) {
-    # Assign production, reserves, and export matrix
-    trade_dat_RTA <- list(P = Prod, R = R1_RTA, E = E0)
-    # Number of countries
-    trade_dat_RTA$nc <-length(trade_dat_RTA$P)
-    # Change in reserves
-    trade_dat_RTA$dR <-rep(0, trade_dat_RTA$nc)
-    # Compute consumption assuming that it is initially equal to supply
-    trade_dat_RTA$C <- get_supply(trade_dat_RTA)
-    # Initial shortage is 0
-    trade_dat_RTA$shortage <-
-      rep(0, trade_dat_RTA$nc)
-    } else {
-    # Clear trade_dat dataframe
-    rm(trade_dat_RTA)
-    # Assign production, reserves *from previous timestep*, and export matrix
-    #   i.e. update only reserves; leave consumption & trade at initial levels
-    trade_dat_RTA <- list(P = Prod, R = R1_RTA, E = E0)
-    # Number of countries
-    trade_dat_RTA$nc <- length(trade_dat_RTA$P)
-    # Change in reserves
-    trade_dat_RTA$dR <- rep(0, trade_dat_RTA$nc)
-    # Initial shortage is 0
-    trade_dat_RTA$shortage <- rep(0, trade_dat_RTA$nc)
-    # Compute consumption assuming that it is initially equal to supply
-    trade_dat_RTA$C <- get_supply(trade_dat_RTA)
-    # Clear results from previous timestep
-    rm(results_FSC_RTA)
-    }
-  
-  # Call main simulation functions
-  results_FSC_RTA <- sim_cascade_RTA(trade_dat_RTA, dP)  # Run Reserves-based Trade Allocation (RTA) Model
-  
+  dP <- as.numeric(unlist(Shocks$dP))
+  if (FSCversion == 0) {
+    results_FSC <-
+      sim_cascade_PTA(trade_dat, dP)  # Run Proportional Trade Allocation (PTA) Model
+  } else if (FSCversion == 1) {
+    results_FSC <-
+      sim_cascade_RTA(trade_dat, dP)  # Run Reserves-based Trade Allocation (RTA) Model
+  }
+
   # Calculate and store outputs of interest from results ====
-  ## PTA version
-  P_PTA[, i + 1]    <- results_FSC_PTA$P
-  R_PTA[, i + 1]    <- results_FSC_PTA$R
-  E_PTA[, , i + 1]  <- results_FSC_PTA$E
-  shortage_PTA[, i] <- results_FSC_PTA$shortage
-  C_C0_PTA[, i] <-
-    results_FSC_PTA$C / trade_dat_PTA$C # consumption relative to initial consumption
-  dR_C0_PTA[, i] <-
-    results_FSC_PTA$dR / trade_dat_PTA$C # change in reserves relative to initial consumption
-  
-  ## RTA version
-  P_RTA[, i + 1]    <- results_FSC_RTA$P
-  R_RTA[, i + 1]    <- results_FSC_RTA$R
-  E_RTA[, , i + 1]  <- results_FSC_RTA$E
-  shortage_RTA[, i] <- results_FSC_RTA$shortage
-  C_C0_RTA[, i] <-
-    results_FSC_RTA$C / trade_dat_RTA$C # consumption relative to initial consumption
-  dR_C0_RTA[, i] <-
-    results_FSC_RTA$dR / trade_dat_RTA$C # change in reserves relative to initial consumption
+  P[, i + 1]    <- results_FSC$P
+  R[, i + 1]    <- results_FSC$R
+  E[, , i + 1]  <- results_FSC$E
+  shortage[, i] <- results_FSC$shortage
+  C_C0[, i] <-
+    results_FSC$C / trade_dat$C # consumption relative to initial consumption
+  dR_C0[, i] <-
+    results_FSC$dR / trade_dat$C # change in reserves relative to initial consumption
 }
