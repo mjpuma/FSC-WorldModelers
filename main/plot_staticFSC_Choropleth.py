f37a3c753e3a73ceaaaaa45f388f14c68e151d41
Puma <puma.nasa@gmail.com>
2021-08-24 18:52:08 -0400

    update to main_static.R and 2021_C2P2 scenarios

--- main/plot_staticFSC_Choropleth.py
+++ main/plot_staticFSC_Choropleth.py
@@ -23,14 +23,127 @@ from scipy import stats
 import matplotlib as plt
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-
-# Specify  crop
-crop_name = 'Wheat'
-#crop_name = 'Rice'
-#crop_name = 'Maize'
-timewindow ='_Avg20152017'
 dir_main = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
-dir_exp = '/Users/puma/Coronavirus/GitHub_codes_data/'
+
+# Specify  crop and scenario
+i_scenario = 34 # See list below 
+
+if (i_scenario == 1):
+   crop_name = 'Wheat'
+   timewindow ='_Avg20152017'
+   outfolder ='COVID-19_data/data_network/'
+   dir_exp = '/Users/puma/Coronavirus/GitHub_codes_data/'
+  
+elif (i_scenario == 2):
+   crop_name = 'Maize'
+   timewindow ='_Avg20152017'
+   outfolder ='COVID-19_data/data_network/'
+   dir_exp = '/Users/puma/Coronavirus/GitHub_codes_data/'
+
+elif (i_scenario == 3):
+   crop_name = 'Rice'
+   timewindow ='_Avg20152017'
+   outfolder ='COVID-19_data/data_network/'
+   dir_exp = '/Users/puma/Coronavirus/GitHub_codes_data/'
+
+# Wheat scenarios (2021) for breadbasket failure and locust declines 
+elif (i_scenario == 11):
+   crop_name = 'Wheat'
+   timewindow ='_breadbasket_Year2008'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+ 
+elif (i_scenario == 12):
+   crop_name = 'Wheat'
+   timewindow ='_locust_Year2020'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
+elif (i_scenario == 13):
+   crop_name = 'Wheat'
+   timewindow ='_locust_Year2020_breadbasket_Year2008'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/' 
+
+elif (i_scenario == 14):
+   crop_name = 'Wheat'
+   timewindow ='_ExportRestrictionFraction_Year2008'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
+elif (i_scenario == 15):
+   crop_name = 'Wheat'
+   timewindow ='_ExportRestrictionFraction_Year2008with_RUS_25'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
+elif (i_scenario == 16):
+   crop_name = 'Wheat'
+   timewindow ='_ExportRestrictionFraction_Year2008with_RUS_50'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
+# Maize scenarios (2021) for breadbasket failure and locust declines 
+elif (i_scenario == 21):
+   crop_name = 'Maize'
+   timewindow ='_breadbasket_Year2008'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+ 
+elif (i_scenario == 22):
+   crop_name = 'Maize'
+   timewindow ='_locust_Year2020'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
+elif (i_scenario == 23):
+   crop_name = 'Maize'
+   timewindow ='_locust_Year2020_breadbasket_Year2008'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/' 
+
+elif (i_scenario == 24):
+   crop_name = 'Maize'
+   timewindow ='_ExportRestrictionFraction_Year2008'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
+elif (i_scenario == 25):
+   crop_name = 'Maize'
+   timewindow ='_ExportRestrictionFraction_Year2008with_ARG_25'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
+elif (i_scenario == 26):
+   crop_name = 'Maize'
+   timewindow ='_ExportRestrictionFraction_Year2008with_ARG_50'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
+elif (i_scenario == 31):
+   crop_name = 'Rice'
+   timewindow ='_breadbasket_Year2008'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
+elif (i_scenario == 32):
+   crop_name = 'Rice'
+   timewindow ='_locust_Year2020'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
+elif (i_scenario == 33):
+   crop_name = 'Rice'
+   timewindow ='_locust_Year2020_breadbasket_Year2008'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
+elif (i_scenario == 34):
+   crop_name = 'Rice'
+   timewindow ='_ExportRestrictionFraction_Year2008'
+   outfolder ='outputs/data_network/'
+   dir_exp = '/Users/puma/GitHub_mjpuma/FSC-WorldModelers/'
+
 
 #Read shapefile for countries using Geopandas
 shapefile = (dir_main+'data/countries_110m/ne_110m_admin_0_countries.shp')
@@ -43,7 +156,7 @@ gdf.columns = ['country', 'country_code', 'geometry']
 gdf = gdf.loc[~(gdf['country'] == 'Antarctica')]
 
 #Read csv file using pandas
-datafile = (dir_exp+'COVID-19_data/data_network/'+ crop_name + timewindow + 'ProductionStatic.csv')
+datafile = (dir_exp + outfolder + crop_name + timewindow + 'ProductionStatic.csv')
 data_production = pd.read_csv(datafile)
 data_production.rename(columns={"iso3": "Code"}, inplace=True)
 data_production["Value"].describe()# stats
@@ -51,44 +164,44 @@ data_production.Value=data_production.Value/1e12 # Convert from kcal to 10^12 kc
 #stats.describe(data_production.Value)
 
 #Read csv file using pandas
-datafile = (dir_exp+'COVID-19_data/data_network/'+ crop_name + timewindow + 'ReserveStatic.csv')
+datafile = (dir_exp+ outfolder + crop_name + timewindow + 'ReserveStatic.csv')
 data_reserve = pd.read_csv(datafile)
 data_reserve.rename(columns={"iso3": "Code"}, inplace=True)
 data_reserve["Value"].describe()# stats
 data_reserve.Value=data_reserve.Value/1e12 # Convert from kcal to 10^12 kcal
 
 #Read csv file using pandas
-datafile = (dir_exp+'COVID-19_data/data_network/'+ crop_name + timewindow + 'ShortageStatic.csv')
+datafile = (dir_exp+ outfolder + crop_name + timewindow + 'ShortageStatic.csv')
 data_shortage = pd.read_csv(datafile)
 data_shortage.rename(columns={"iso3": "Code"}, inplace=True)
 data_shortage.Value=data_shortage.Value/1e12 # Convert from kcal to 10^12 kcal
 data_shortage["Value"].describe()# stats
 
 #Read csv file using pandas
-datafile = (dir_exp+'COVID-19_data/data_network/'+ crop_name + timewindow + 'ExportStatic.csv')
+datafile = (dir_exp+ outfolder + crop_name + timewindow + 'ExportStatic.csv')
 data_export = pd.read_csv(datafile, header = 0, index_col = 0)
 num_yrs = int(data_export.shape[1]/data_export.shape[0])
 data_export_2darr = data_export.values 
 
 #Read csv file using pandas
-datafile = (dir_exp+'COVID-19_data/data_network/'+ crop_name + timewindow + 'C1C0Static.csv')
+datafile = (dir_exp+ outfolder + crop_name + timewindow + 'C1C0Static.csv')
 data_C1toC0 = pd.read_csv(datafile)
 data_C1toC0.rename(columns={"iso3": "Code"}, inplace=True)
 data_C1toC0["Value"].describe()# stats
 
 #Read csv file using pandas
-datafile = (dir_exp+'COVID-19_data/data_network/'+ crop_name + timewindow + 'C2C0Static.csv')
+datafile = (dir_exp+ outfolder + crop_name + timewindow + 'C2C0Static.csv')
 data_C2toC0 = pd.read_csv(datafile)
 data_C2toC0.rename(columns={"iso3": "Code"}, inplace=True)
 data_C2toC0["Value"].describe()# stats
 
-#Read csv file using pandas
-datafile = (dir_exp+'COVID-19_data/data_network/GlobalMetrics/'+ crop_name + timewindow + 'Gdeg_in_kcal.csv')
-data_degreein = pd.read_csv(datafile)
-data_degreein.rename(columns={"iso3": "Code"}, inplace=True)
-data_degreein.rename(columns={"Gdeg_in": "Value"}, inplace=True)
-data_degreein.Value=data_degreein.Value # Convert from kcal to 10^12 kcal
-data_degreein["Value"].describe()# stats
+# #Read csv file using pandas
+# datafile = (dir_exp+'COVID-19_data/data_network/GlobalMetrics/'+ crop_name + timewindow + 'Gdeg_in_kcal.csv')
+# data_degreein = pd.read_csv(datafile)
+# data_degreein.rename(columns={"iso3": "Code"}, inplace=True)
+# data_degreein.rename(columns={"Gdeg_in": "Value"}, inplace=True)
+# data_degreein.Value=data_degreein.Value # Convert from kcal to 10^12 kcal
+# data_degreein["Value"].describe()# stats
 
 #Plot C1C0Static
 df_plot = data_C1toC0
@@ -99,13 +212,13 @@ ax = merged.plot(column="Value",cmap='OrRd_r',legend=True,scheme="natural_breaks
             );
 description = '''
 Based on FAOSTAT Data'''.strip()
-ax.set_title(title, fontdict={'fontsize': 20}, loc='left')
+ax.set_title(title, fontdict={'fontsize': 14}, loc='left')
 ax.annotate(description, xy=(0.1, 0.1), size=12, xycoords='figure fraction')
 ax.set_axis_off()
 ax.get_legend().set_bbox_to_anchor((.12, .4))
 ax.get_figure()
-plt.pyplot.savefig((dir_exp+'COVID-19_data/figs/C1toC0_'+ crop_name + '.png'))
-plt.pyplot.savefig((dir_exp+'COVID-19_data/figs/C1toC0_'+ crop_name + '.svg'))
+plt.pyplot.savefig((dir_exp+'figures/C1toC0_'+ crop_name + timewindow + '.png'))
+#plt.pyplot.savefig((dir_exp+'figures/C1toC0_'+ crop_name + timewindow + '.svg'))
 
 
 #Plot C2C0Static
@@ -117,10 +230,10 @@ ax = merged.plot(column="Value",cmap='OrRd_r',legend=True,scheme="natural_breaks
             );
 description = '''
 Based on FAOSTAT Data'''.strip()
-ax.set_title(title, fontdict={'fontsize': 20}, loc='left')
+ax.set_title(title, fontdict={'fontsize': 14}, loc='left')
 ax.annotate(description, xy=(0.1, 0.1), size=12, xycoords='figure fraction')
 ax.set_axis_off()
 ax.get_legend().set_bbox_to_anchor((.12, .4))
 ax.get_figure()
-plt.pyplot.savefig((dir_exp+'COVID-19_data/figs/C2toC0_'+ crop_name + '.png'))
-plt.pyplot.savefig((dir_exp+'COVID-19_data/figs/C2toC0_'+ crop_name + '.svg'))
+plt.pyplot.savefig((dir_exp+'figures/C2toC0_'+ crop_name + timewindow + '.png'))
+#plt.pyplot.savefig((dir_exp+'figures/C2toC0_'+ crop_name + timewindow + '.svg'))
